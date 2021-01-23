576e24f09eee016fa3d1bf03c361bae932c66617
kylemocode <kylemo860617@gmail.com>
2021-01-23 15:43:43 +0800

    feat: strict any check

--- src/index.ts
+++ src/index.ts
@@ -4,45 +4,67 @@ import table from 'text-table';
 import cheerio from 'cheerio';
 require('dotenv').config();
 
+const CLAPS_COUNT_REGEX = /\d+(\.\d{1,2})?K?\s?/;
+const MAX_STR_LENGTH = 25;
 const MEDIUM_API_BASE_URL =
   'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@';
 const MEDIUM_PROFILE_BASE_URL = 'https://medium.com/@';
 
+interface IMappedData {
+  title: string;
+  claps: string;
+}
+
+type ItemData = {
+  title: string,
+  guid: string,
+  categories: string[],
+}
+
+interface APIResponse {
+  items: ItemData[];
+}
+
 (async () => {
-  const { GIST_ID, GH_PAT, MEDIUM_USER_NAME } = process.env;
-  let apiResponse: AxiosResponse<any>;
-  let followerCount;
-  let slicedData;
-  let articlesContent: unknown[][] = [];
+  const {
+    GIST_ID, 
+    GH_PAT, 
+    MEDIUM_USER_NAME 
+  } = process.env;
+  let apiResponse: AxiosResponse<APIResponse>;
+  let followerCount: string;
+  let slicedData: IMappedData[];
+  let articlesContent: string[][] = [];
 
   if (!GIST_ID || !GH_PAT || !MEDIUM_USER_NAME) return;
 
-  // Get user's medium data
   try {
+    // Get user's medium data from rss API
     apiResponse = await axios.get(MEDIUM_API_BASE_URL + MEDIUM_USER_NAME);
+
+    // Use cheerio to get articles' claps count
     slicedData = await Promise.all(apiResponse.data.items
-      .filter(item => item.categories.length !== 0)
-      .slice(0, 3)
+      .filter(item => item.categories.length !== 0) // filter comment
+      .slice(0, 3) // latest 3 articles
       .map(async item => {
         const res = await axios.get(item.guid);
         const $ = cheerio.load(res.data);
         const text = $('button').text();
-        let matches = text.match(/\d+(\.\d{1,2})?K?\s?/); 
-        return { title: item.title, claps: matches ? matches[0] : 0 };
+        let matches = text.match(CLAPS_COUNT_REGEX); 
+        return { title: item.title, claps: matches ? matches[0] : '0' };
       }));
-    console.log(apiResponse);
   } catch (err) {
-    console.error(err);
+    throw new Error('Get data failed');
   }
 
+  // Get user's follower count
   const res = await axios.get(MEDIUM_PROFILE_BASE_URL + MEDIUM_USER_NAME);
   const $ = cheerio.load(res.data);
-  //@ts-ignore
   followerCount = $('a')['3'].children[0].data;
 
   slicedData.forEach(item => {
     let trimTitle;
-    if (item.title.length > 25) trimTitle = item.title.slice(0, 25) + '...';
+    if (item.title.length > MAX_STR_LENGTH) trimTitle = item.title.slice(0, MAX_STR_LENGTH) + '...';
     else trimTitle = item.title;
     articlesContent.push([trimTitle, `👏${item.claps}`]);
   })
@@ -51,7 +73,7 @@ const MEDIUM_PROFILE_BASE_URL = 'https://medium.com/@';
     [
       [`@${MEDIUM_USER_NAME}`, followerCount],
       ['Latest Articles', '👇'],
-      ...articlesContent as string[][],
+      ...articlesContent,
     ],
     { align: ['l', 'r'], stringLength: () => 20 }
   );
@@ -61,6 +83,6 @@ const MEDIUM_PROFILE_BASE_URL = 'https://medium.com/@';
   try {
     await box.update({ filename: 'medium-stat.md', content: gistContent });
   } catch (err) {
-    console.error(err);
+    throw new Error('Update gist failed')
   }
 })();
