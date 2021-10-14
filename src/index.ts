5a2f26908bc962f8c1d49b97814ef194b873444c
kylemocode <kylemo860617@gmail.com>
2021-10-14 18:01:54 +0800

    update regex for user who has followers higher than 1K

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import table from 'text-table';
 import cheerio from 'cheerio';
 require('dotenv').config();
 
-const FOLLOWERS_COUNT_REGEX = /\d+\sFollowers/;
+const FOLLOWERS_COUNT_REGEX = /[\d+A-Z?]*\sFollowers/;
 const CLAPS_COUNT_REGEX = /\d+(\.\d{1,2})?K?\s?/;
 const MAX_STR_LENGTH = 25;
 const MEDIUM_API_BASE_URL =
@@ -17,21 +17,17 @@ interface IMappedData {
 }
 
 type ItemData = {
-  title: string,
-  guid: string,
-  categories: string[],
-}
+  title: string;
+  guid: string;
+  categories: string[];
+};
 
 interface APIResponse {
   items: ItemData[];
 }
 
 (async () => {
-  const {
-    GIST_ID, 
-    GH_PAT, 
-    MEDIUM_USER_NAME 
-  } = process.env;
+  const { GIST_ID, GH_PAT, MEDIUM_USER_NAME } = process.env;
   let apiResponse: AxiosResponse<APIResponse>;
   let followerCount: string;
   let slicedData: IMappedData[];
@@ -44,16 +40,18 @@ interface APIResponse {
     apiResponse = await axios.get(MEDIUM_API_BASE_URL + MEDIUM_USER_NAME);
 
     // Use cheerio to get articles' claps count
-    slicedData = await Promise.all(apiResponse.data.items
-      .filter(item => item.categories.length !== 0) // filter comment
-      .slice(0, 3) // latest 3 articles
-      .map(async item => {
-        const res = await axios.get(item.guid);
-        const $ = cheerio.load(res.data);
-        const text = $('button').text();
-        let matches = text.match(CLAPS_COUNT_REGEX); 
-        return { title: item.title, claps: matches ? matches[0] : '0' };
-      }));
+    slicedData = await Promise.all(
+      apiResponse.data.items
+        .filter((item) => item.categories.length !== 0) // filter comment
+        .slice(0, 3) // latest 3 articles
+        .map(async (item) => {
+          const res = await axios.get(item.guid);
+          const $ = cheerio.load(res.data);
+          const text = $('button').text();
+          let matches = text.match(CLAPS_COUNT_REGEX);
+          return { title: item.title, claps: matches ? matches[0] : '0' };
+        })
+    );
   } catch (err) {
     throw new Error('Get data failed');
   }
@@ -61,18 +59,21 @@ interface APIResponse {
   // Get user's follower count
   const res = await axios.get(MEDIUM_PROFILE_BASE_URL + MEDIUM_USER_NAME);
   const $ = cheerio.load(res.data);
-  const followerCountMatchList= $('a').text().match(FOLLOWERS_COUNT_REGEX);
-  followerCount = followerCountMatchList ? followerCountMatchList[0] : '0 Follower';
+  const followerCountMatchList = $('a').text().match(FOLLOWERS_COUNT_REGEX);
+  followerCount = followerCountMatchList
+    ? followerCountMatchList[0]
+    : '0 Follower';
 
-  slicedData.forEach(item => {
+  slicedData.forEach((item) => {
     let trimTitle;
-    if (item.title.length > MAX_STR_LENGTH) trimTitle = item.title.slice(0, MAX_STR_LENGTH) + '...';
+    if (item.title.length > MAX_STR_LENGTH)
+      trimTitle = item.title.slice(0, MAX_STR_LENGTH) + '...';
     else trimTitle = item.title;
     articlesContent.push([trimTitle, `👏 ${item.claps}`]);
-  })
+  });
 
   if (slicedData.length === 0) {
-    articlesContent.push(['I have no posts in Medium currently...', '😢'])
+    articlesContent.push(['I have no posts in Medium currently...', '😢']);
   }
 
   const gistContent = table(
@@ -89,6 +90,6 @@ interface APIResponse {
   try {
     await box.update({ filename: 'medium-stat.md', content: gistContent });
   } catch (err) {
-    throw new Error('Update gist failed')
+    throw new Error('Update gist failed');
   }
 })();
